49f3e4130fcf33ff9e6c8f8be9fd5f4da656ac4d
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 19:36:03 -0500

    p0

--- gen/edu/uchicago/cs/gerber/layout/R.java
+++ gen/edu/uchicago/cs/gerber/layout/R.java
@@ -20,12 +20,6 @@ public final class R {
         public static final int button4=0x7f050003;
         public static final int button5=0x7f050004;
         public static final int button6=0x7f050005;
-        public static final int editText1=0x7f05000a;
-        public static final int textView1=0x7f050006;
-        public static final int textView2=0x7f050007;
-        public static final int textView3=0x7f050008;
-        public static final int toggleButton1=0x7f050009;
-        public static final int toggleButton2=0x7f05000b;
     }
     public static final class layout {
         public static final int grid=0x7f030000;
